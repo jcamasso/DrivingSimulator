9833c4049c88ee85a5d2a1e18964f9fbc5a169fe
Justin Camasso <camassojustin@yahoo.com>
2017-02-01 00:49:36 -0500

    Menu Screen update 1

---
+++ Driving Simulator/Assets/Menu/UIManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class UIManager : MonoBehaviour {
+
+    public EventSystem ES;
+    private GameObject storeSelected;
+
+    void Start()
+    {
+        storeSelected = ES.firstSelectedGameObject;
+    }
+
+    void Update()
+    {
+        if (ES.currentSelectedGameObject != storeSelected)
+        {
+            if (ES.currentSelectedGameObject == null)
+            {
+                ES.SetSelectedGameObject(storeSelected);
+            }
+            else
+            {
+                storeSelected = ES.currentSelectedGameObject;
+                
+            }
+        }
+    }
+
+    public void LevelOneStart()
+    {
+        SceneManager.LoadScene("Cars");
+    }
+
+    public void LevelTwoStart()
+    {
+        SceneManager.LoadScene("Cars");
+    }
+
+    public void LevelThreeStart()
+    {
+        SceneManager.LoadScene("Cars");
+    }
+
+
+}
